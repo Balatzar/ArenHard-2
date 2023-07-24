ca9b51c5929c3c5afc6949ee425b4210c0cb30e1
Blatalzar <bataprod@gmail.com>
2023-07-24 18:06:52 +0200

    Add sci fi perso

--- Gladiator/gladiator.gd
+++ Gladiator/gladiator.gd
@@ -24,10 +24,16 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction == 1:
+		animator.flip_h = false
+	elif direction == -1:
+		animator.flip_h = true
+		
 	if direction:
 		velocity.x = direction * SPEED
 		animator.play("Run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animator.play("Idle")
 
 	move_and_slide()
