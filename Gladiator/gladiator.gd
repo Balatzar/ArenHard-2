da2bc45eec83d62bb74fbd25d1238d2263c510f6
Blatalzar <bataprod@gmail.com>
2023-07-24 15:51:22 +0200

    Add basic player

--- Gladiator/gladiator.gd
+++ Gladiator/gladiator.gd
@@ -4,9 +4,13 @@ extends CharacterBody2D
 @export var SPEED = 300.0
 @export var JUMP_VELOCITY = -400.0
 
+@onready var animator = get_node("AnimatedSprite2D")
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func _ready():
+	animator.play("Idle")
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -22,6 +26,7 @@ func _physics_process(delta):
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		animator.play("Run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
