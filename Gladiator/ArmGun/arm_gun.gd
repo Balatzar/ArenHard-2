0196708d7eec1b5666f99eb5516d2e1eaff3c6ba
voncapel <chapelle.tristan@gmail.com>
2023-07-26 00:01:15 +0200

    jump on FLEEK
    You can now hold the spacebar to jump a lil' extra bit higher - well done

--- Gladiator/ArmGun/arm_gun.gd
+++ Gladiator/ArmGun/arm_gun.gd
@@ -13,19 +13,16 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var mouse_pos = get_global_mouse_position()
-	print(rotation)
 	# look_at(mouse_pos)
 	
 	var angle = atan2(mouse_pos.y - global_position.y, mouse_pos.x - global_position.x)
 	
 	rotation = angle
 	if angle > max_rotation_up && angle < max_rotation_down:
-		print("no rotato")
 		scale.y = 1
 		owner.flip(1)
 		# rotation = angle + .5
 	else:
-		print("rotato")
 		scale.y = -1
 		owner.flip(-1)
 		# rotation = angle - .5
